1109f00667f2cdfbdc45fbb21cd0ea0e5f11744e
Sarah Soo <sarah.jing.ting.soo@citrix.com>
2020-09-11 16:17:01 +0100

    Can solve row/column with 2 numbers missing

--- main.py
+++ main.py
@@ -1,111 +1,139 @@
-from subgrid import Subgrid
 from puzzle import Puzzle
-from row import Row
 import numpy as np
 
-def find_least_missing_row(puzzle):
-    row_n = 10
-    lowest = 9
+def find_least_missing(container):
+    n = 10
+    lowest = 10
     count = 0
     i = 0
     j = 0
 
     while (i < 9):
         while (j < 9):
-            if puzzle[i,j] == 0:
-                count += 1
+            if container == "row":
+                if puzzle[i,j] == 0:
+                    count += 1
+            elif container == "column":
+                if puzzle[j,i] == 0:
+                    count += 1
             j += 1
         if (count != 0) and (count < lowest):
             lowest = count
-            row_n = i
+            n = i
         i += 1
         j = 0
         count = 0
 
-    return row_n, lowest
+    if lowest == 10:
+        lowest = 0
+    else:
+        print("Found " + container + " with " + str(lowest) + " missing number(s)." )
 
-# def find_least_missing_column(puzzle):
-#     column_n = 0
-#     lowest = 9
-#     count = 0
-#     i = 0
-#     j = 0
+    return n, lowest
 
-#     while (i < 9):
-#         while (j < 9):
-#             if puzzle[j,i] == 0:
-#                 count += 1
-#             j += 1
-#         if (count != 0) and (count < lowest):
-#             lowest = count
-#             column_n = i
-#         i += 1
-#         j = 0
-#         count = 0
-
-#     return column_n, lowest
-
-def find_missing_values_from_row(puzzle, row_n):
+def find_missing_values(n, container):
     i = 0
     missing = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     while (i < 9):
         j = 1
         while (j <= 9):
-            if puzzle[row_n,i] == j:
-                missing.remove(j)
-                break
+            if container == "row":
+                if puzzle[n,i] == j:
+                    missing.remove(j)
+                    break
+            elif container == "column":
+                if puzzle[i,n] == j:
+                    missing.remove(j)
+                    break
             j += 1
         i += 1
+
     return missing
 
-# def find_missing_values_from_column(puzzle, column_n):
-#     i = 0
-#     missing = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-#     while (i < 9):
-#         j = 1
-#         while (j <= 9):
-#             if puzzle[i,column_n] == j:
-#                 missing.remove(j)
-#                 break
-#             j += 1
-#         i += 1
-#     return missing
-
-def solve_row(row_n, missing):
-    col_with_empty = 0
+def already_exists(n, m, missing, container):
     i = 0
-    while (i < 9):
-        if puzzle[row_n,i] == 0:
-            col_with_empty = i
-            break
-        i += 1
     
-    puzzle[row_n,col_with_empty] = missing[0]
+    if container == "row":
+        while (i < 9):
+            if puzzle[i,m] == missing:
+                return True
+            i += 1
+        return False
+    elif container == "column":
+        while (i < 9):
+            if puzzle[m,i] == missing:
+                return True
+            i += 1
+        return False
 
-# def solve_column(column_n, missing):
-#     row_with_empty = 0
-#     i = 0
-#     while (i < 9):
-#         if puzzle[i,column_n] == 0:
-#             row_with_empty = i
-#             break
-#         i += 1
+
+def solve(n, missing, container):
+    print("Solving " + container + " with " + str(len(missing)) + " missing cell(s).")
+
+    m = 0       # corresponding row/column that references missing number
+    i = 0
+
+    if len(missing) == 1:
+        while (i < 9):
+            if container == "row":
+                if puzzle[n,i] == 0:
+                    m = i
+                    break
+                i += 1
+            elif container == "column":
+                if puzzle[i,n] == 0:
+                    m = i
+                    break
+                i += 1
         
-#         puzzle[row_with_empty,column_n] = missing[0]
+        if container == "row":
+            puzzle[n,m] = missing[0]
+        elif container == "column":
+            puzzle[m,n] = missing[0]
+    elif len(missing) == 2:
+        while (i < 9):
+            if container == "row":
+                if puzzle[n,i] == 0:
+                    m = i
+                    break
+                i += 1
+            elif container == "column":
+                if puzzle[i,n] == 0:
+                    m = i
+                    break
+                i += 1
+                
+        if container == "row":
+            if not already_exists(n, m, missing[0], container):
+                puzzle[n,m] = missing[0]
+            else:
+                puzzle[n,m] = missing[1]
+        elif container == "column":
+            if not already_exists(n, m, missing[0], container):
+                puzzle[m,n] = missing[0]
+            else:
+                puzzle[m,n] = missing[1]
 
 print("Loading puzzle...")
 p = Puzzle()
-puzzle = p.load_puzzle("C:\\repos\\sudoku-solver\\test_full_1.txt")
+puzzle = p.load_puzzle("C:/repos/sudoku-solver/test_full_4.txt")
 
-row_n, r_lowest = find_least_missing_row(puzzle)
+row_n, r_lowest = find_least_missing("row")
+column_n, c_lowest = find_least_missing("column")
 
-while row_n != 10:
-    missing = find_missing_values_from_row(puzzle, row_n)
-    solve_row(row_n, missing)
-    row_n, r_lowest = find_least_missing_row(puzzle)
+while row_n != 10 or column_n != 10:
+    while r_lowest <= c_lowest and row_n != 10:
+        missing = find_missing_values(row_n, "row")
+        solve(row_n, missing, "row")
+        row_n, r_lowest = find_least_missing("row")
+        column_n, c_lowest = find_least_missing("column")
 
-# column_n, c_lowest = find_least_missing_column(puzzle)
+    while r_lowest > c_lowest and column_n != 10:
+        missing = find_missing_values(column_n, "column")
+        solve(column_n, missing, "column")
+        row_n, r_lowest = find_least_missing("row")
+        column_n, c_lowest = find_least_missing("column")
+   
 
 print(puzzle)
